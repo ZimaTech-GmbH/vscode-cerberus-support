629d67b5b5a14b54729e6dfab98ae97ac546d10f
Holzchopf <oli4stuker@hotmail.com>
2020-06-25 16:30:13 +0200

    CHANGED identifier, doc ADDED configuration
    CHANGED cerbers-x identifier now unified to cerberus-x in package.json
    
    CHANGED documentation webview now shows current path and allows navigating back. Also added prototype of image support
    
    ADDED CxConfiguration singleton

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as fs from 'fs';
+import { TextDecoder } from 'util';
 
 import { CxDocumentSymbolProvider } from './features/documentSymbolProvider';
+import { CxConfiguration } from './features/configuration';
 
 var decls: any;
 var curdecl: any;
+var _webview: any;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-cerberusx-support" is now active!');
+	// register functions to be called once configuration is valid
+	CxConfiguration.onConfigurationValid(() => {
+		loadDecls();
+	});
+
+	// check if Cerberus X configuration is valid
+	CxConfiguration.validate();
+
+	vscode.workspace.onDidChangeConfiguration((event) => {
+		CxConfiguration.validate();
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-cerberusx-support.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('cerberus-x.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
@@ -29,7 +39,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('vscode-cerberusx-support.showDocumentation', () => {
+		vscode.commands.registerCommand('cerberus-x.showDocumentation', () => {
 			// Create web panel
 			let panel = vscode.window.createWebviewPanel(
 				'cxDocumentation',
@@ -43,23 +53,17 @@ export function activate(context: vscode.ExtensionContext) {
 					enableScripts: true,
 					enableCommandUris: true,
 					enableFindWidget: true,
-					localResourceRoots: [vscode.Uri.parse( "file://C:/Software/Cerberus/docs/html", true )]
+					localResourceRoots: [vscode.Uri.file( CxConfiguration.get('path') )]
 				}
 			)
 			const onDiskPath: vscode.Uri = vscode.Uri.file("C:/Software/Cerberus/docs/html/data/mojo2/graphics/bb_additive.png");
 			const imgSource: vscode.Uri = panel.webview.asWebviewUri(onDiskPath);
 			// panel.webview.html = getWebviewContent(imgSource);
 
+			_webview = panel.webview;
 			panel.webview.html = getWebviewContent2();
 
-			// panel.webview.onDidReceiveMessage(
-			// 	message => {
-			// 		vscode.window.showErrorMessage(message.text);
-			// 		console.log(message);
-			// 	},
-			// 	undefined,
-			// 	context.subscriptions
-			// );
+			// simple navigation
 			panel.webview.onDidReceiveMessage(
 				message => {
 					panel.webview.html = getWebviewContent2(message.text);
@@ -70,46 +74,26 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.languages.registerDocumentSymbolProvider(
-			{ scheme: 'file', language: 'cerberus' },
+			{ scheme: 'file', language: 'cerberus-x' },
 			new CxDocumentSymbolProvider()
 		)
 	);
-
-	loadDecls();
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
-
-function getWebviewContent(imgSource: vscode.Uri) {
-
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-	  <img src="${imgSource}" width="300" />
-  </body>
-  </html>`;
-}
 
 function getWebviewContent2( page: string = '' ) {
 	let html: string;
 
 	let d: any = curdecl;
 
-	// navigate
-	if (d.uid != page) {
-		for (let c of d.childs) {
-			if (c.uid === page) {
-				curdecl = c;
-				d = curdecl;
-				break;
-			}
-		}
+	if (page == '') {
+		curdecl = decls;
+		d = curdecl;
+	} else {
+		curdecl = getDeclByUid(decls, page);
+		d = curdecl;
 	}
 
 	let title: string = d.kind + ': ' + d.ident;
@@ -121,6 +105,7 @@ function getWebviewContent2( page: string = '' ) {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<base href="docs/html">
 	<title>${title}</title>
 	<script>
 		const vscode = acquireVsCodeApi();
@@ -130,18 +115,45 @@ function getWebviewContent2( page: string = '' ) {
 	</script>
 </head>
 <body>
+	<header>
+		You are here 
+`;
+	// build navigation path
+	let path: any[] = [d];
+	let c = d;
+	while (c.parent) {
+		path.unshift(c.parent);
+		c = c.parent;
+	}
+	for (const c of path) {
+		html +=
+`		&raquo;&nbsp;<span onClick="navigate('${c.uid}')" style="cursor: pointer;">${c.kind}: ${c.ident}</span>&emsp;
+`;
+	}
+		
+	html +=
+`	</header>
 	<h1>${title}</h1>
 	<small>${uid}</small>
 	<h2>Contents</h2>
 	<ul>
 `;
 	
-	for (let c of d.childs) {
-		html +=
-`		<li onClick="navigate('${c.uid}')">${c.kind}: ${c.ident}</li>
+	if (d.childs) {
+		for (let c of d.childs) {
+			let target = c.target || c.uid;
+			let content: string = c.ident;
+			content = content.replace(/!\[([^\]]*)\]\(([^\)]*)\)/g, (match, p1, p2) => {
+				const src = _webview.asWebviewUri(vscode.Uri.file(CxConfiguration.get('path') + '/docs/html/' + p2));
+				return `<img alt="${p1}" src="${src}" />`;
+			})
+			html +=
+`		<li onClick="navigate('${target}')">${c.uid}, ${c.kind}: ${content}</li>
 `;
+		}
+	
 	}
-
+	
 	html +=
 `	</ul>
 	</body>
@@ -152,16 +164,44 @@ function getWebviewContent2( page: string = '' ) {
 
 // load decls.json
 function loadDecls() {
-	fs.readFile('C:\\Software\\Cerberus\\docs\\html\\decls.json', 'utf8', (err, data) => {
-		if (err) {
-			vscode.window.showErrorMessage(err.message);
-		} else if (data) {
-			let d: any = JSON.parse(data);
+	const uri = vscode.Uri.file(CxConfiguration.get('path') + '/docs/html/decls.json');
+	vscode.workspace.fs.readFile(uri).then(
+		(contents) => {
+			let d: any = JSON.parse(new TextDecoder().decode(contents));
 			if (d.kind === "root") {
+				// set global decls to currently loaded one
 				decls = d;
+				// current (navigated to) decls
 				curdecl = decls;
+				// set parent decl for every decl
+				setThisChildsParentDecl(decls);
 				vscode.window.showInformationMessage('Cerberus X decls loaded');
 			}
+		},
+		(err) => {
+			vscode.window.showErrorMessage(err.message, 'Rebuild Help');
 		}
-	})
+	);
+}
+
+function setThisChildsParentDecl(decl: any): void {
+	if (!decl.childs) return;
+	// set parent decl for every decl
+	for (const c of decl.childs) {
+		c.parent = decl;
+		setThisChildsParentDecl(c);
+	}
+}
+
+function getDeclByUid(decl: any, uid: string): any {
+	if (decl.uid === uid) return decl;
+	if (!decl.childs) return null;
+	for (const c of decl.childs) {
+		if (c.uid === uid) {
+			return c;
+		}
+		const d = getDeclByUid(c, uid);
+		if (d) return d;
+	}
+	return null;
 }
---
+++ src/features/configuration.ts
@@ -0,0 +1,81 @@
+import * as vscode from 'vscode';
+import { TextDecoder } from 'util';
+
+export class CxConfiguration {
+  private static onConfigurationValidCallbacks: any[] = [];
+
+  // get and set Cerberus X configurations
+  public static get(section: string): any {
+    return vscode.workspace.getConfiguration('cerberusX').get(section);
+  }
+  public static set(section: string, value: any): Thenable<void> {
+    return vscode.workspace.getConfiguration('cerberusX').update(section, value, true);
+  }
+
+  // define functions to be called when configuration is valid
+  public static onConfigurationValid(callback: any): void {
+    this.onConfigurationValidCallbacks.push(callback);
+  }
+
+  // helper functions
+
+  // validate Cerberus X configuration
+  public static validate(): void {
+    const path = this.get('path');
+    // no path set = invalid path
+    if (!path) {
+      this.messageInvalidPath('No Cerberus X path set.');
+      return;
+    }
+    // look for VERSIONS.TXT
+    const vfile = vscode.Uri.file(path + '/VERSIONS.TXT');
+    vscode.workspace.fs.readFile(vfile).then(
+      // VERSIONS.TXT existing, try to extract current version
+      (contents) => {
+        let text: string = new TextDecoder().decode(contents);
+        let version = text.match(/^[\*]{5}\s([^\*]*)\s[\*]{5}/);
+        if (!version) {
+          vscode.window.showInformationMessage('Could not retrieve current version from VERSIONS.TXT');
+        } else {
+          vscode.window.showInformationMessage('Cerberus X ' + version[1] + ' up and running!');
+        }
+        // in either case, call on*valid callbacks
+        for (const callback of this.onConfigurationValidCallbacks) {
+          callback();
+        }
+      },
+      // no VERSIONS.TXT = invalid path
+      () => {
+        this.messageInvalidPath('Cerberus X path set to invalid target.');
+      }
+    );
+  }
+
+  // inform user about invalid cerberus path
+  private static messageInvalidPath(message: string): void {
+    const itemSetPath: string = 'Set path'
+    vscode.window.showErrorMessage(message, itemSetPath).then((action) => {
+      // set path via system dialog, if user selects action
+      if (action === itemSetPath) {
+        this.selectPath();
+      }
+    });
+  }
+
+  public static async selectPath() {
+    // show path selection dialog
+    const options: vscode.OpenDialogOptions = {
+      canSelectMany: false,
+			canSelectFolders: true,
+			canSelectFiles: false,
+			openLabel: 'Select'
+    }
+    vscode.window.showOpenDialog(options).then(async (path) => {
+      // when path was selected, set in config and check config again
+      if (path && path[0]) {
+        await this.set('path', path[0].fsPath);
+        this.validate();
+      }
+    });
+  }
+}
